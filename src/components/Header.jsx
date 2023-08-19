a592a2e76dbea6c2d64381fa8d69fb325d1d9f38
Riyaj Hossain <a.mrh.riyaj@gmail.com>
2023-08-19 15:14:55 +0600

    Try

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Stories from "./Stories"
 const Header = () => {
     return(
         <div className="header-body d-flex">
-            {[1,2,3,4,5,6,7].map((k="number") => (
+            {[1,2,3,4,5,6,7,8].map((k="number") => (
                 <div key={k}>
                     <Stories />
                 </div>
